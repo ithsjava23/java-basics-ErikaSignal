894c0af3e7fcde6846e37f0a4a51b6bdabe9e724
erika <erika.signal@gmail.com>
2023-09-06 13:00:33 +0200

    meny funkar, case 1 klar, case 2 påbörjad.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,4 +1,5 @@
 package org.example;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class App {
@@ -7,6 +8,7 @@ public class App {
 
         String val;
         Scanner sc = new Scanner(System.in);
+        int[] priser = new int [24];
 
         do {
             System.out.println("Elpriser");
@@ -20,7 +22,6 @@ public class App {
 
             switch (val) {
                 case "1":
-                    int[] priser = new int [24];
                     for (int i = 0; i < 24; i++) {
                         System.out.print("Ange pris på elen i hela ören per kW/h för timintervall " + i + "-" + (i + 1) + ": ");
                         int pris = sc.nextInt();
@@ -30,7 +31,26 @@ public class App {
                     System.out.println("Priser är nu lagrade.");
                     break;
                 case "2":
-                    //ToDo
+                    int min = priser[0];
+                    int minIndex = 0;
+                    for (int i = 0; i < priser.length; i++) {
+                        if(min > priser[i]){
+                            min = priser[i];
+                            minIndex = i;
+                        }
+                    }
+                    int max = priser[0];
+                    int maxIndex = 0;
+                    for (int i = 0; i < priser.length; i++) {
+                        if(max < priser[i]){
+                            max = priser[i];
+                            maxIndex = i;
+                        }
+                    }
+                    int mean;
+                    System.out.println("Det lägsta priset är: " + min + " kr per kW/h mellan klockan " + minIndex + "-" + (minIndex + 1));
+                    System.out.println("Det högsta priset är: " + max + " kr per kW/h mellan klockan " + maxIndex + "-" + (maxIndex + 1));
+                    System.out.println("Medelvärdet av priset per kW/h under dygnet är: ");
                     break;
                 case "3":
                     //ToDo: Sortera koden efter dyrast till billigast.
