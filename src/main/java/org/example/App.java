0cd27de93636119249885597ce852b7ef90cc711
erika <erika.signal@gmail.com>
2023-09-07 17:31:40 +0200

    meny funkar, case 1,2 och 3 klar.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -20,7 +20,7 @@ public class App {
             val = sc.nextLine();
 
             switch (val) {
-                case "1":
+                case "1" -> {
                     for (int i = 0; i < 24; i++) {
                         System.out.print("Ange pris på elen i hela ören per kW/h för timintervall " + i + "-" + (i + 1) + ": ");
                         int pris = sc.nextInt();
@@ -28,12 +28,12 @@ public class App {
                         priser[i] = pris;
                     }
                     System.out.println("Priser är nu lagrade.");
-                    break;
-                case "2":
+                }
+                case "2" -> {
                     int min = priser[0];
                     int minIndex = 0;
                     for (int i = 0; i < priser.length; i++) {
-                        if(min > priser[i]){
+                        if (min > priser[i]) {
                             min = priser[i];
                             minIndex = i;
                         }
@@ -41,18 +41,19 @@ public class App {
                     int max = priser[0];
                     int maxIndex = 0;
                     for (int i = 0; i < priser.length; i++) {
-                        if(max < priser[i]){
+                        if (max < priser[i]) {
                             max = priser[i];
                             maxIndex = i;
                         }
                     }
                     double mean = Arrays.stream(priser).average().getAsDouble();
-                    System.out.println("Det lägsta priset är: " + min + " öre per kW/h mellan klockan " + minIndex + "-" + (minIndex + 1));
-                    System.out.println("Det högsta priset är: " + max + " öre per kW/h mellan klockan " + maxIndex + "-" + (maxIndex + 1));
+                    System.out.println("Det lägsta priset är: " + min + " öre per kW/h vid timintervall " + minIndex + "-" + (minIndex + 1));
+                    System.out.println("Det högsta priset är: " + max + " öre per kW/h vid timintervall " + maxIndex + "-" + (maxIndex + 1));
                     System.out.println("Medelvärdet av priset per kW/h under dygnet är: " + mean + " öre");
-                    break;
-                case "3":
-                    List<Sort> elements = new ArrayList<Sort>();
+                }
+                case "3" -> {
+                    System.out.println("Priser sorterade från dyrast till billigast pris per kW/h inklusive dess timintervall:");
+                    List<Sort> elements = new ArrayList<>();
                     for (int i = 0; i < priser.length; i++) {
                         elements.add(new Sort(i, priser[i]));
                     }
@@ -61,16 +62,13 @@ public class App {
                     for (Sort element : elements) {
                         System.out.println(element.index + "-" + (element.index + 1) + " " + element.value + " öre");
                     }
-                    break;
-                case "4":
-                    //ToDo: Bästa/billigaste 4 timmar i sträck.
-                    break;
-                case "e", "E":
-                    break;
-                default:
-                    System.out.println("Fel inmatning, kontrollera och försök igen.");
+                }
+                case "4" -> {
+                }
+                case "e", "E" -> {}
+                default -> System.out.println("Fel inmatning, kontrollera och försök igen.");
             }
         } while(!"e".equalsIgnoreCase(val));
-        System.out.println("avsluta");
+        System.out.println("Programmet har avslutats");
     }
 }
